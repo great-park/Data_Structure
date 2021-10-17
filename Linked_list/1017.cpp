14430ec58683a20171929937f1717f49ade0560a
great-park <cksgh1735@naver.com>
2021-10-17 14:13:50 +0900

    doubly linked list

---
+++ Linked_list/1017.cpp
@@ -0,0 +1,49 @@
+# include<stdio.h>
+# include<stdlib.h>
+
+typedef struct node {
+	int value;
+	struct node* next;
+	struct node* prev;
+} Node;
+
+typedef struct list {
+	Node* first=NULL;
+	Node* last=NULL;
+} List;
+
+void print_list(List *l)
+{
+	for (Node* t = l->first; t != NULL; t = t->next) {
+		printf("%d ->", t->value);
+	}
+	printf("\n");
+}
+
+void push_last(List *l, int value)
+{
+	Node* newNode = (Node*)malloc(sizeof(Node));
+	//newNode는 지역변수라서 삭제, malloc으로 할당한 메모리 공간은 유지
+	newNode->value = value;
+	newNode->next = NULL;
+	newNode->prev = l->last;
+
+	if (l->last == NULL) {
+		l->first = l->last = newNode;
+	}
+	else {
+		l->last->next = newNode;
+		l->last = newNode;
+	}
+}
+
+int main()
+{
+	List random;
+	
+	push_last(&random, 7);
+	push_last(&random, 8);
+	push_last(&random, 9);
+
+	return 0;
+}

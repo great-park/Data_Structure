10b490e3c22377ade2c6485b369e19737fb6c29d
great-park <cksgh1735@naver.com>
2022-01-01 22:21:23 +0900

    트리 순회 - 블로그 참고

--- Tree/BST- 삽입, 검색, 삭제, 순회4가지.py
+++ Tree/BST- 삽입, 검색, 삭제, 순회4가지.py
@@ -6,30 +6,33 @@ class Node:
 
 
 class BST(Node):  # 상속
+    def __init__(self):
+        self.root = None
+
     def insert(self, data):
-        self.root = self.insert_value(self.root, data)
+        self.root = self._insert_value(self.root, data)
         return self.root is not None
 
-    def insert_value(self, node, data):
+    def _insert_value(self, node, data):
         if node is None:
             node = Node(data)
         else:
             if data <= node.data:
-                node.left = self.insert_value(node.left, data)
+                node.left = self._insert_value(node.left, data)
             else:
-                node.right = self.insert_value(node.right, data)
+                node.right = self._insert_value(node.right, data)
         return node
 
     def find(self, key):
-        return self.finde_value(self.root, key)
+        return self._find_value(self.root, key)
 
-    def find_value(self, root, key):
+    def _find_value(self, root, key):
         if root is None or root.data == key:
-            return root
+            return root is not None
         elif key < root.data:
-            return self.find_value(root.left, key)
+            return self._find_value(root.left, key)
         else:
-            return self.find_value(root.right, key)
+            return self._find_value(root.right, key)
 
     def delete(self, key):
         self.root, deleted = self._delete_value(self.root, key)
@@ -40,12 +43,10 @@ class BST(Node):  # 상속
             return node, False
 
         deleted = False
-        # 해당 노드가 삭제할 노드일 경우
         if key == node.data:
             deleted = True
-            # 삭제할 노드가 자식이 두개일 경우
             if node.left and node.right:
-                # 오른쪽 서브 트리에서 가장 왼쪽에 있는 노드를 찾고 교체
+                # replace the node to the leftmost of node.right
                 parent, child = node, node.right
                 while child.left is not None:
                     parent, child = child, child.left
@@ -54,10 +55,8 @@ class BST(Node):  # 상속
                     parent.left = child.right
                     child.right = node.right
                 node = child
-            # 자식 노드가 하나일 경우 해당 노드와 교체
             elif node.left or node.right:
                 node = node.left or node.right
-            # 자식 노드가 없을 경우 그냥 삭제
             else:
                 node = None
         elif key < node.data:
@@ -65,3 +64,63 @@ class BST(Node):  # 상속
         else:
             node.right, deleted = self._delete_value(node.right, key)
         return node, deleted
+
+    def pre_order_traversal(self):
+        def _pre_order_traversal(root):
+            if root is None:
+                pass
+            else:
+                print(root.data)
+                _pre_order_traversal(root.left)
+                _pre_order_traversal(root.right)
+        _pre_order_traversal(self.root)
+
+    def in_order_traversal(self):
+        def _in_order_traversal(root):
+            if root is None:
+                pass
+            else:
+                _in_order_traversal(root.left)
+                print(root.data)
+                _in_order_traversal(root.right)
+        _in_order_traversal(self.root)
+
+    def post_order_traversal(self):
+        def _post_order_traversal(root):
+            if root is None:
+                pass
+            else:
+                _post_order_traversal(root.left)
+                _post_order_traversal(root.right)
+                print(root.data)
+        _post_order_traversal(self.root)
+
+    def level_order_traversal(self):  # BFS
+        def _level_order_traversal(root):
+            queue = [root]
+            while queue:
+                root = queue.pop(0)
+                if root is not None:
+                    print(root.data)
+                    if root.left:
+                        queue.append(root.left)
+                    if root.right:
+                        queue.append(root.right)
+        _level_order_traversal(self.root)
+
+
+array = [40, 4, 34, 45, 14, 55, 48, 13, 15, 49, 47]
+
+bst = BST()
+for x in array:
+    bst.insert(x)
+
+# depth first
+bst.pre_order_traversal()   # 40 4 34 14 13 15 45 55 48 47 49
+print("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ")
+bst.in_order_traversal()    # 4 13 14 15 34 40 45 47 48 49 55
+print("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ")
+bst.post_order_traversal()  # 13 15 14 34 4 47 49 48 55 45 40
+print("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ")
+# breadth first
+bst.level_order_traversal()  # 40 4 45 34 55 14 48 13 15 47 49

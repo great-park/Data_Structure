be0e70f8eb62ba2423ddcb868f3700ee299f2b84
great-park <cksgh1735@naver.com>
2022-01-24 14:28:55 +0900

    kruskal, prim

--- Graph/Graph 간단 구현.py
+++ Graph/Graph 간단 구현.py
@@ -11,3 +11,12 @@ graph2 = graph("c", 3)
 # 노드들을 파이썬 리스트에 저장
 
 node_list = [graph0, graph1, graph2]
+graph[0]
+
+# 노드들을 파이썬 딕셔너리에 저장
+node_dic = {
+    "a": graph0,
+    "b": graph1,
+    "c": graph2
+}
+node_dic["a"]
